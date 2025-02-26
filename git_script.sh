0bc3da890444b763401b2cd50890244b7040492c
FelipeGasp <felipe.gasparotto@sptech.school>
2025-02-26 10:44:54 -0300

    Feature: V1 formulário com validações simples

---
+++ git_script.sh
@@ -0,0 +1,8 @@
+echo "# react-forms-example" >> README.md
+git init
+git add README.md
+git commit -m "Enviando arquivos ao repositório"
+git branch -M main
+git remote add origin https://github.com/FelipeGasp/react-forms-example.git
+git push -u origin main
+
